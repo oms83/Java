b013e7f972ead73923049566d6e9f161b5af240d
Omer MEMES <omer.memes20@gmail.com>
2024-11-19 20:32:15 +0300

    Scanner class and simple example

---
+++ src/SCANNER.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class SCANNER {
+    public static class Person
+    {
+        public String FirstName;
+        public String LastName;
+        public short Age;
+        public String Address;
+
+        public Person(){}
+
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Person person = new Person();
+
+        GetPersonInfo(person, sc);
+
+        PrintPersonInfo(person);
+    }
+
+    private static void PrintPersonInfo(Person person) {
+        System.out.println(person.FirstName + " " + person.LastName);
+        System.out.println(person.Age + " " + person.Address);
+    }
+
+    private static void GetPersonInfo(Person person, Scanner sc) {
+        System.out.println("Enter First Name: ");
+        person.FirstName = sc.nextLine();
+
+        System.out.println("Enter Last Name: ");
+        person.LastName = sc.nextLine();
+
+        System.out.println("Enter Age: ");
+        person.Age = sc.nextShort();
+        sc.nextLine();
+
+        System.out.println("Enter Address: ");
+        person.Address = sc.nextLine();
+    }
+}
