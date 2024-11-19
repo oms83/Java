fcda87a6fc9eaf69647b565346847513a0d1cf2f
Omer MEMES <omer.memes20@gmail.com>
2024-11-20 02:38:07 +0300

    Oop Without Polymorphism

--- src/OopWithoutPolymorphism.java
+++ src/OopWithoutPolymorphism.java
@@ -1,8 +1,30 @@
 public class OopWithoutPolymorphism {
 
+    public static class Shape
+    {
+        private int x;
 
+        public Shape(int x)
+        {
+            this.x = x;
+        }
+
+        public void SquareArea(int x)
+        {
+            System.out.println("Square area of " + x + " is " + this.x * this.x);
+        }
+
+        public void CircleArea(int x)
+        {
+            System.out.println("Circle area of " + x + " is " + this.x * this.x * Math.PI);
+        }
+    }
 
     public static void main(String[] args) {
+
+        Shape s = new Shape(5);
+        s.SquareArea(5);
+        s.CircleArea(5);
 
     }
 }
