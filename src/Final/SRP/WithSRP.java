de11c40007d6c371cb33eb734dd9988e12e4cc31
Ömer Memes <omer.memes20@gmail.com>
2025-01-24 01:16:41 +0300

    SRP

---
+++ src/Final/SRP/WithSRP.java
@@ -0,0 +1,44 @@
+package Final.SRP;
+
+public class WithSRP {
+    public static class NotificationService
+    {
+        enum enServiceType {SMS, FAX, EMAIL}
+        public void Notify(String to, String body, enServiceType serviceType) {
+            if (NotificationService.enServiceType.EMAIL == serviceType) {
+                EmailService.SendEmail(to, body);
+            }
+            else if (NotificationService.enServiceType.SMS == serviceType) {
+                SMSService.SendSMS(to, body);
+            }
+            else {
+                FaxService.SendFax(to, body);
+            }
+        }
+    }
+
+    public static class EmailService {
+        public static void SendEmail(String to, String body) {
+            System.out.println("To: " + to + " Body: " + body);
+        }
+    }
+
+    public static class SMSService {
+        public static void SendSMS(String to, String body) {
+            System.out.println("To: " + to + " Body: " + body);
+        }
+    }
+
+    public static class FaxService {
+        public static void SendFax(String to, String body) {
+            System.out.println("To: " + to + " Body: " + body);
+        }
+    }
+
+    public static void main(String[] args) {
+        NotificationService service = new NotificationService();
+        service.Notify("Ömer MEMES", "Hi, Ömer Baboş", NotificationService.enServiceType.EMAIL);
+        service.Notify("Ömer MEMES", "Hi, Ömer Baboş", NotificationService.enServiceType.SMS);
+        service.Notify("Ömer MEMES", "Hi, Ömer Baboş", NotificationService.enServiceType.FAX);
+    }
+}
---
+++ src/Final/SRP/WithoutSRP.java
@@ -0,0 +1,36 @@
+package Final.SRP;
+
+public class WithoutSRP {
+
+    public static class NotificationService
+    {
+        enum enServiceType {SMS, FAX, EMAIL}
+        public void Notify(String to, String body, enServiceType serviceType) {
+            if (enServiceType.EMAIL == serviceType) {
+                _SendEmail(to, body);
+            }
+            else if (enServiceType.SMS == serviceType) {
+                _SendSMS(to, body);
+            }
+            else {
+                _SendFax(to, body);
+            }
+        }
+        private void _SendEmail(String to, String body) {
+            System.out.println("To: " + to + " Body: " + body);
+        }
+        private void _SendFax(String to, String body) {
+            System.out.println("To: " + to + " Body: " + body);
+        }
+        private void _SendSMS(String to, String body) {
+            System.out.println("To: " + to + " Body: " + body);
+        }
+    }
+
+    public static void main(String[] args) {
+        NotificationService service = new NotificationService();
+        service.Notify("Ömer MEMES", "Hi, Ömer Baboş", NotificationService.enServiceType.EMAIL);
+        service.Notify("Ömer MEMES", "Hi, Ömer Baboş", NotificationService.enServiceType.SMS);
+        service.Notify("Ömer MEMES", "Hi, Ömer Baboş", NotificationService.enServiceType.FAX);
+    }
+}
